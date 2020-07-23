4d4ef7baf01a6ce84da31aefe33d3f56755ad9d4
120061203 <ccssll120061203@gmail.com>
2020-07-23 22:40:29 +0800

    animate left height opacity width queue

--- jquery.js
+++ jquery.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
     $(".div5").click(function() {
         $(".div6").slideToggle("slow");
     });
+
+
+    $(".btnAnimate").click(function() {
+        $(".box1").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: 'toggle',
+            width: '150px'
+        });
+    });
+
+    $(".btnAnimateQueue").click(function() {
+        var div = $(".box1");
+        div.animate({ height: '300px', opacity: '0.4' }, "slow");
+        div.animate({ width: '300px', opacity: '0.8' }, "slow");
+        div.animate({ height: '100px', opacity: '0.4' }, "slow");
+        div.animate({ width: '100px', opacity: '0.8' }, "slow");
+    });
+
 });
