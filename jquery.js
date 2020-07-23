bed43655d211056f6fad34ab2a54cfa18659466c
120061203 <ccssll120061203@gmail.com>
2020-07-23 13:51:13 +0800

    slideDown exercise

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".div1").click(function() {
+        $(".div2").slideDown("slow");
+    });
+    $(".div3").click(function() {
+        $(".div4").slideDown("slow");
+    });
+    $(".div5").click(function() {
+        $(".div6").slideDown("slow");
+    });
+});
